41941b54ecfc69f7b3aa70d9bb805b8fef2d5e3e
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-15 15:46:20 -0800

    Update home page layout and styling

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex">
-      <div className="w-1/2 flex flex-col justify-center items-center text-center px-32">
-        <h1>Your Virtual Bookkeeping Solution</h1>
-        <p>Get the expert small business bookkeeping services you need, so you can run your business. Let ApexSheets take bookkeeping tasks off your plate so you can focus on more relevant business needs. I can clean up your existing financials to bring them up to date, and continue to manage your books moving forward. Free up your workload, and save yourself from stress during tax time with accurate maintained ledgers</p>
+      <div className="w-1/2 flex flex-col justify-center space-y-8 items-center py-8 px-32">
+        <h1 className={`text-4xl text-apex-green`}>Your Virtual Bookkeeping Solution</h1>
+        <p className={`text-sm text-gray-600`}>Get the expert small business bookkeeping services you need, so you can run your business. Let ApexSheets take bookkeeping tasks off your plate so you can focus on more relevant business needs. I can clean up your existing financials to bring them up to date, and continue to manage your books moving forward. Free up your workload, and save yourself from stress during tax time with accurate maintained ledgers</p>
         <button>Learn More</button>
       </div>
-      <div className="w-1/2 min-w-[500px] min-h-[400px] relative">
-        <Image src="/Home-Page.webp" alt="Home page image" layout="fill" objectFit="cover"/>
+      <div className="w-1/2 relative">
+        <Image src="/Home-Page.webp" alt="Home page image" layout="fill" objectFit="contain"/>
       </div>
     </div>
   )
