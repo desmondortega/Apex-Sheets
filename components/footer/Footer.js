7ef452f924b55c05f6091f7cfee81c6e01d38117
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-15 13:52:07 -0800

    Created footer component

---
+++ components/footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="flex flex-col items-center justify-center">
+            <p className="mb-1 text-xxs">Desmond Ortega</p>
+            <a href="mailto:info@apexsheets.com" className="mb-1 text-xxs">info@apexsheets.com</a>
+            <p className="text-xxs">© 2023 Apex Sheets. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
