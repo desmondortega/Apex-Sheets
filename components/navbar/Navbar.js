94efa8e0f801a57088c50518184e4a4de34b5296
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-15 11:58:57 -0800

    Changed styling of navbar to match wix

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({ isMobile, isOpen, setIsOpen }) => {
         <>
             {isMobile && 
                 <button 
-                    className="fixed top-0 right-0 mx-12 my-8 bg-transparent z-10"
+                    className="fixed top-0 right-0 ml-12 mr-16 my-8 bg-transparent z-10"
                     onClick={() => setIsOpen(!isOpen)}
                 >   
                     {isOpen ? <FiX className="h-8 w-8 text-gray-500" /> : <FiMenu className="h-8 w-8 text-gray-500" />}
@@ -14,19 +14,19 @@ const Navbar = ({ isMobile, isOpen, setIsOpen }) => {
             }
             {isOpen && (
                 <div>
-                    <nav className={`${isMobile ? 'fixed inset-0 flex flex-col items-center bg-white space-y-4 h-screen mt-16 w-full text-gray-500' : 'flex items-center justify-between text-gray-500'}`}>
-                        <img src="/logo-color.png" alt="Logo" className="h-40" />
-                        <ul className={`${isMobile ? 'flex flex-col space-y-8' : 'flex space-x-8'} list-none`}>
-                            <li className={`${isMobile ? 'text-2xl' : ''} inline-block mx-4 no-underline`}>
+                    <nav className={`${isMobile ? 'fixed inset-0 flex flex-col items-center bg-white space-y-4 h-screen mt-16 pr-8 w-full text-gray-500' : 'flex items-center justify-between text-gray-500'}`}>
+                        <img src="/logo-color.png" alt="Logo" className={`${isMobile ? "h-56" : "h-40"}`} />
+                        <ul className={`${isMobile ? 'flex flex-col space-y-20 text-center' : 'flex space-x-16'} list-none`}>
+                            <li className={`${isMobile ? 'text-3xl' : 'text-lg'} inline-block mx-4 no-underline`}>
                                 <Link href="/">Home</Link>
                             </li>
-                            <li className={`${isMobile ? 'text-2xl' : ''} inline-block mx-4 no-underline`}>
+                            <li className={`${isMobile ? 'text-3xl' : 'text-lg'} inline-block mx-4 no-underline`}>
                                 <Link href="/about">About</Link>
                             </li>
-                            <li className={`${isMobile ? 'text-2xl' : ''} inline-block mx-4 no-underline`}>
+                            <li className={`${isMobile ? 'text-3xl' : 'text-lg'} inline-block mx-4 no-underline`}>
                                 <Link href="/service">Services & Pricing</Link>
                             </li>
-                            <li className={`${isMobile ? 'text-2xl' : ''} inline-block mx-4 no-underline`}>
+                            <li className={`${isMobile ? 'text-3xl' : 'text-lg'} inline-block mx-4 no-underline`}>
                                 <Link href="/contact">Contact</Link>
                             </li>
                         </ul>
