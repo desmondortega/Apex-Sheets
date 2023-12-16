1fb4d99d4bd63d582bc561d0e4ccc88c0f305078
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-15 21:30:40 -0800

    Link href change to /about

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="w-full sm:w-1/2 flex flex-col justify-center space-y-10 items-center sm:py-20 sm:px-32 pb-20 px-8 sm:min-w-[650px] min-w-[100px]">
           <h1 className={`text-4xl text-apex-green`}>Your Virtual Bookkeeping Solution</h1>
           <p className={`text-sm text-gray-600`}>Get the expert small business bookkeeping services you need, so you can run your business. Let ApexSheets take bookkeeping tasks off your plate so you can focus on more relevant business needs. I can clean up your existing financials to bring them up to date, and continue to manage your books moving forward. Free up your workload, and save yourself from stress during tax time with accurate maintained ledgers</p>
-          <Link href="about" className={`text-sm text-gray-100 bg-apex-green py-3 px-5 rounded`}>Learn More</Link>
+          <Link href="/about" className={`text-sm text-gray-100 bg-apex-green py-3 px-5 rounded`}>Learn More</Link>
       </div>
       <div className="sm:w-1/2 sm:min-w-[450px] min-w-[100px] m-12">
         <Image src="/Home-Page.webp" alt="Home page image" width={1288} height={1072} className='shadow-2xl rounded-xl'/>
