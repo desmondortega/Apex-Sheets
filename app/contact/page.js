cc9af4ac4610f4a102892b1688d1a30b7e3dfe8d
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-15 23:31:55 -0800

    Contact page under construction

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,5 @@
 export default function Contact() {
     return (
-        <div>Contact</div>
+        <div className={"flex justify-center items-center text-center"}>UNDER CONSTRUCTION</div>
     )
 }
