1a9589b569551a6ef68eeab6594d2b5ed2ce4c52
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2023-12-16 20:06:19 -0800

    center justified home paragraph in mobile view

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="sm:mr-24 sm:ml-12 sm:min-w-[1100px] flex flex-col sm:flex-row sm:text-left text-center">
       <div className="w-full sm:w-1/2 flex flex-col justify-center space-y-10 items-center sm:py-20 sm:px-32 pb-20 px-8 sm:min-w-[650px] min-w-[100px]">
           <h1 className={`sm:text-6xl text-4xl text-apex-green`}>Your Virtual Bookkeeping Solution</h1>
-          <p className={`sm:text-lg text-sm sm:text-left text-gray-600`}>Get the expert small business bookkeeping services you need, so you can run your business. Let ApexSheets take bookkeeping tasks off your plate so you can focus on more relevant business needs. I can clean up your existing financials to bring them up to date, and continue to manage your books moving forward. Free up your workload, and save yourself from stress during tax time with accurate maintained ledgers</p>
+          <p className={`sm:text-lg text-sm sm:text-left text-center-justify text-gray-600`}>Get the expert small business bookkeeping services you need, so you can run your business. Let ApexSheets take bookkeeping tasks off your plate so you can focus on more relevant business needs. I can clean up your existing financials to bring them up to date, and continue to manage your books moving forward. Free up your workload, and save yourself from stress during tax time with accurate maintained ledgers</p>
           <Link href="/about" className={`sm:text-lg text-sm text-gray-100 bg-apex-green py-3 px-5 rounded`}>Learn More</Link>
       </div>
       <div className="sm:w-1/2 sm:min-w-[450px] min-w-[100px] m-4">
